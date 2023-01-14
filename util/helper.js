1c8027af9fb7a3f6aefad83bff6cc0c19161cbd8
pho86@my.bcit.ca <pho86@my.bcit.ca>
2023-01-13 22:44:03 -0800

    description for selectRandomFromArray function

--- util/helper.js
+++ util/helper.js
@@ -1,3 +1,8 @@
+/**
+ * @desc selectRandomFromArray(), selects a random value from an array of values
+ * @param {*} array takes in an array
+ * @returns a random value from the array
+ */
 export function selectRandomFromArray(array) {
    let randomValue = array[Math.floor(Math.random() * array.length)]
    return randomValue
